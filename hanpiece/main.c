bc3e061eed0b592d8cea589044a87e397a9cc6f2
ppickle <kng8765@naver.com>
2023-04-10 15:13:21 +0900

    주석 추가 및 체력 출력 수정

--- hanpiece/main.c
+++ hanpiece/main.c
@@ -32,7 +32,9 @@ int luffy_turn(Player* luffy, Player* crocodile) {
 	case 1:
 		printf("\n루피의 공격 : '고무고무 총!'\n");
 		if (crocodile->is_sand == 1) {
+			printf("크로커다일: %d - %d = %d\n", crocodile->hp, luffy->atk / 2, crocodile->hp - luffy->atk / 2);
 			crocodile->hp -= luffy->atk / 2;
+
 		}
 		else {
 			crocodile->hp -= luffy->atk;
@@ -75,22 +77,27 @@ int crocodile_turn(Player* luffy, Player* crocodile) {
 
 int game(int turn, Player* luffy, Player* crocodile) {
 	while (1) {
+		//turn == 1 루피 일때
 		if (turn == 1) {
+			//루피 턴 함수 실행
 			luffy_turn(luffy, crocodile);
-			if (crocodile->is_sand == 1) {
+			//크로커다일이 모래로 변신했을 때
+			if (crocodile->is_sand == 1) {   
 				printf("Luffy      HP: %d\n", (luffy->hp < 0 ? 0 : luffy->hp));
 				printf("Crocodile  HP: %d _크로커다일의 방어로 데미지가 반감됩니다.\n", (crocodile->hp < 0 ? 0 : crocodile->hp));
-				//printf("크로커다일: %d - %d = %d",)
-
 			}
 			else {
 				printf("Luffy      HP: %d\n", (luffy->hp < 0 ? 0 : luffy->hp));
 				printf("Crocodile  HP: %d\n", (crocodile->hp < 0 ? 0 : crocodile->hp));
 			}
+			//크로커다일 turn으로 바뀜
 			turn = 2;
 		}
+		// turn ==2 크로커다일 일때
 		else if (turn == 2) {
+			//크로커다일 턴 함수 실행
 			crocodile_turn(luffy, crocodile);
+			//크로커다일이 모래로 변신했을때
 			if (crocodile->is_sand == 1) {
 				printf("Luffy      HP: %d\n", (luffy->hp < 0 ? 0 : luffy->hp));
 				printf("Crocodile  HP: %d, 모래로 변신\n", (crocodile->hp < 0 ? 0 : crocodile->hp));
@@ -99,6 +106,7 @@ int game(int turn, Player* luffy, Player* crocodile) {
 				printf("Luffy      HP: %d\n", (luffy->hp < 0 ? 0 : luffy->hp));
 				printf("Crocodile  HP: %d\n", (crocodile->hp < 0 ? 0 : crocodile->hp));
 			}
+			//루피 turn으로 바뀜
 			turn = 1;
 		}
 		
@@ -127,20 +135,25 @@ int game(int turn, Player* luffy, Player* crocodile) {
 
 
 int main(void) {
+	//intro
 	printf("==========한피스 시작==========\n");
 	printf("해적왕이 될 사나이 루피는 \n고잉 메리호를 타고 길을 떠난다.\n\n바다를 떠돌던 중\n칠무해 크로커다일을 만나 전투가 벌어지는데...\n강적을 만난 루피!\n과연 이길 수 있을 것인가?\n두둥!\n\n");
 
+	//시작 Enter 입력
 	printf("Press <Enter> to continue...\n\n");
 	while (getchar() != '\n');
 
+	//난수
 	srand(time(NULL));
 
+	//플레이어 랜덤 hp,atk 
 	Player luffy = { RandomNumber(20, 40), RandomNumber(5,15) };
 	Player crocodile = { RandomNumber(30, 50), RandomNumber(15,25) };
 
 	printf("Luffy      HP: %d, 공격력: %d\n", luffy.hp, luffy.atk);
 	printf("Crocodile  HP: %d, 공격력: %d\n", crocodile.hp, crocodile.atk);
 
+	//선공 선택
 	int turn = 0;  // 1이면 루피 선공, 2이면 크로커다일 선공
 	while (turn != 1 && turn != 2) {
 		printf("\n선공을 선택하세요 (1: 루피, 2: 크로커다일): ");
@@ -152,9 +165,10 @@ int main(void) {
 			turn = 0;  // 입력값 초기화
 		}
 	}
-
+	//게임 실행 함수
 	game(turn,&luffy,&crocodile);
 
+	//승패 결정 난 후 게임 재시작 여부 입력받기
 	while (1) {
 		printf("재시작하시겠습니까? (1: 재시작 / 0: 종료) >>> ");
 		int restart;
@@ -176,7 +190,6 @@ int main(void) {
 			exit(0);
 		}
 	}
-
 
 	return 0;
 }
